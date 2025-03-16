194c09250781e63f2d20966fdacca6d1dc0eb8d8
harishkumar59 <127928933+harishkumar59@users.noreply.github.com>
2025-03-16 07:41:56 +0000

    innintial

---
+++ newone.js
@@ -0,0 +1,54 @@
+
+
+//let name = "hello";
+//console.log(name);
+
+
+//var age = 23;
+//console.log(age);
+
+// don't use var for variables 
+//only use let 
+
+let newName = "ajay new name ";
+const h = "old name";
+
+const mm = "neeew";
+
+console.log(h);
+console.log(newName)
+
+console.log(newName)
+console.log(newName)
+ //DATA TYPES 
+
+
+ //premitive dadta type 7
+ // 1. number 
+//  2. string 
+//  3. bolean
+// 4.  undefined 
+// 5. null
+//6. bign int
+
+
+// NON PRIMEITIVE 
+
+// OBJECTS  KEY VALUE  PAIRS 
+// ARRAYS 
+// 
+
+
+const student = {
+    fullNames: "rahul",
+    age : 29,
+
+};
+console.log(student);
+console.log(typeof student);
+console.log(student["fullNames"]);
+//console.log(student.age);
+
+student["age"] = student["age"] + 1;
+console.log(student.age);
+console.log("last commit")
